7467597e6312f6008814bd5212531971a206ebc4
2026-DEV2-050 <dev2026identifier@gmail.com>
2026-02-22 14:42:48 +0100

    Implement seconds logic - test succeed.

--- app/src/main/java/com/kata/berlinclock/domain/BerlinClockMapper.kt
+++ app/src/main/java/com/kata/berlinclock/domain/BerlinClockMapper.kt
@@ -1,11 +1,25 @@
 package com.kata.berlinclock.domain
 
 import com.kata.berlinclock.domain.model.BerlinClockState
+import com.kata.berlinclock.domain.model.Lamp
+import com.kata.berlinclock.domain.model.LampColor
+import com.kata.berlinclock.domain.model.LampStatus
 import java.time.LocalTime
 
 object BerlinClockMapper {
 
     fun map(time: LocalTime): BerlinClockState {
-        throw NotImplementedError("Not implemented yet")
+        val secondsOn = time.second % 2 == 0
+
+        return BerlinClockState(
+            secondsLamp = Lamp(
+                color = LampColor.YELLOW,
+                status = if (secondsOn) LampStatus.ON else LampStatus.OFF
+            ),
+            fiveHoursRow = emptyList(),
+            oneHourRow = emptyList(),
+            fiveMinutesRow = emptyList(),
+            oneMinuteRow = emptyList()
+        )
     }
 }
