4cf014d2bd2953f7bf2e53610b295c35beb3c1cb
2026-DEV2-050 <dev2026identifier@gmail.com>
2026-02-22 16:07:17 +0100

    Add BerlinClockViewModel with StateFlow time updates.

---
+++ app/src/main/java/com/kata/berlinclock/presentation/BerlinClockViewModel.kt
@@ -0,0 +1,29 @@
+package com.kata.berlinclock.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kata.berlinclock.domain.BerlinClockMapper
+import com.kata.berlinclock.domain.model.BerlinClockState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.time.Clock
+import java.time.LocalTime
+
+class BerlinClockViewModel (
+    private val clock: Clock = Clock.systemDefaultZone()
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(BerlinClockMapper.map(LocalTime.now(clock)))
+    val state: StateFlow<BerlinClockState> = _state
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                _state.value = BerlinClockMapper.map(LocalTime.now(clock))
+                delay(1000)
+            }
+        }
+    }
+}
